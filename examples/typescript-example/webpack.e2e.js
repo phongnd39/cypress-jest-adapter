8375bd2eea2865c7870bad7d063d487a7ff26d7a
Huang Jian <huang.jian@howtimeflies.io>
2019-04-01 23:55:46 +0800

    Add an example project to test the typescript usage.

---
+++ examples/typescript-example/webpack.e2e.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist')
+  }
+}
