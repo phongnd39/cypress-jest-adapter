a31d6f442b3c3df1b42bff42d714b6f41cbbbfde
Jimmy Multani <jimmy.multani@rakuten.com>
2020-08-05 16:29:13 -0400

    target node in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ const path = require('path')
 
 module.exports = {
   mode: 'production',
+  target: 'node',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    publicPath: '/'
-  }
+    publicPath: '/',
+  },
 }
